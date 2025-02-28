999a441a5b435bbeb514cb21121d288e16036912
reinisa <reinis411@gmail.com>
2025-02-28 16:53:09 +0200

    Added Students List table

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,12 +1,14 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
+
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button, Container, Paper} from '@mui/material';
 
 export default function Student() {
     const paperStyle={padding:'50px 20px', width:600, margin:"20px auto"}
-    const[name,setName]=React.useState('')
-    const[address,setAddress]=React.useState('')
+    const[name,setName]=useState('')
+    const[address,setAddress]=useState('')
+    const[students,setStudents]=useState([])
 
     const handleClick=(e)=>{
         e.preventDefault()
@@ -20,6 +22,14 @@ export default function Student() {
         console.log("New student added")
     })
 }
+
+useEffect(()=>{
+    fetch("http://localhost:8080/student/getAll")
+    .then(res=>res.json())
+    .then((result)=>{
+        setStudents(result)
+    }
+)},[])
 
   return (
     <Container>
@@ -40,6 +50,20 @@ export default function Student() {
             onChange={(e)=>setAddress(e.target.value)}/>
             </Box>
             <Button variant="contained" onClick={handleClick}>Submit</Button>
+        </Paper>
+
+        <h1>Students List</h1>
+
+        <Paper elevation={3} style={paperStyle}>
+
+            {students.map(student=>(
+                <Paper elevation={6} style={{margin:"10px",padding:"15px",textAlign:"left"}} key={student.id}>
+                    Id:{student.id}<br/>
+                    Name:{student.name}<br/>
+                    Address:{student.address}
+                </Paper>
+            ))}
+                
         </Paper>
     </Container>
   );
